b673ac04e14699cc9c75dde2ef5a1891af64748e
bryanjoachim <33373881+bryanjoachim@users.noreply.github.com>
2025-07-28 10:11:20 -0400

    Update ChatController.java

--- src/main/java/com.example.chatbot/controller/ChatController.java
+++ src/main/java/com.example.chatbot/controller/ChatController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
-
+     public String home() {
+        return "Hello from Spring Boot on EC2!";
+    }
     @PostMapping
     public String chat(@RequestBody ChatRequest request) {
         String userMessage = request.getMessage().toLowerCase();
